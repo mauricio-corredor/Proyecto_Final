ae014d8f1ab138fb26c31e369cec64f729ea608c
Veiko Quiroz <v.quiroz@uniandes.edu.co>
2023-03-05 21:33:51 -0500

    Se agrega el Producer en bodega

--- research/bodegas/src/app.module.ts
+++ research/bodegas/src/app.module.ts
@@ -1,10 +1,31 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { SqsModule} from '@ssut/nestjs-sqs';
+import * as AWS from 'aws-sdk';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+AWS.config.update({
+  region: 'us-east-1', // aws region
+  accessKeyId: 'AKIAWONOFRHV4HRWB3V2', // aws access key id
+  secretAccessKey: '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', // aws secret access key
+});
+
 @Module({
-  imports: [],
+  imports: [
+    SqsModule.register({
+      consumers: [],
+      producers: [
+          {
+              name: "ColaInventario", 
+              queueUrl: "https://sqs.us-east-1.amazonaws.com/443283769835/ColaInventario", 
+              region: 'us-east-1', // url of the queue
+          },
+      ],
+  }),
+  ],
   controllers: [AppController],
   providers: [AppService],
+  
 })
 export class AppModule {}
