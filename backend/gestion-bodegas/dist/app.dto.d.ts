5ed6a0415b67b1c5138a88d7f26568559ed9d738
Mauricio Corredor <malec@uniandes.edu.co>
2023-04-16 18:08:09 -0500

    Revert "Adicion de atributos en producto"
    This reverts commit b626dec0af76f38efe9331fda2bc47b51cd0852a.

--- backend/gestion-bodegas/dist/app.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class AppDto {
-    readonly nombreBodega: string;
-    readonly ubicacionPais: string;
-    readonly ubicacionCiudad: string;
-    readonly zonaLocalizacion: string;
-    readonly capacidadVolumen: number;
-    readonly capacidadUsada: number;
-    readonly CapacidadDisponible: number;
-}
