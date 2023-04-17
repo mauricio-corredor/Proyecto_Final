19a970bbad26397b84ead9517013a5a671323d03
Mauricio Corredor <malec@uniandes.edu.co>
2023-04-16 23:34:24 -0500

    Cambio direcciones LB

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'LB-app-bodega-2115956073.us-east-1.elb.amazonaws.com:81'
+  baseUrl: 'http://LB-app-bodega-2115956073.us-east-1.elb.amazonaws.com:81',
+  bodegaUrl: 'http://LB-app-bodega-2115956073.us-east-1.elb.amazonaws.com:80'
 
 };
