9700f85f9729ed8f6ee35c1e1f14b6955cb5c173
mgdarwin <d.moreno11@uniandes.edu.co>
2023-05-20 20:20:19 -0500

    Se ajustan textos y se perfila la generacion del POST para Ordenes - prueba 1

--- mobile/app/src/main/java/com/miso/g2/ccpappmovil/ui/screens/shoppingCart/shoppingCartMainPage.kt
+++ mobile/app/src/main/java/com/miso/g2/ccpappmovil/ui/screens/shoppingCart/shoppingCartMainPage.kt
@@ -1,15 +1,17 @@
 package com.miso.g2.ccpappmovil.ui.screens.shoppingCart
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.miso.g2.ccpappmovil.model.CartItemArticleDetail
 import com.miso.g2.ccpappmovil.ui.theme.BackgroundMain
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -18,52 +20,79 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.miso.g2.ccpappmovil.MyApplication.Companion.orderProductsList
 import com.miso.g2.ccpappmovil.R
+import com.miso.g2.ccpappmovil.model.ProductoOrden
 import com.miso.g2.ccpappmovil.ui.screens.products.NavigationBar
-
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 
 @Composable
 fun ShoppingCartMainPage(navController: NavController) {
+
+    var subTotalOrder = 0.0F
+    var taxOrder = 0.0F
+    var totalOrder = 0.0F
+
+    if (orderProductsList.size > 0) {
+        for (element in orderProductsList) {
+            subTotalOrder += element.valorTotal
+            taxOrder = subTotalOrder * 0.19F
+            totalOrder = subTotalOrder + taxOrder
+        }
+    }
+
     Box(
         modifier = Modifier
             .background(color = BackgroundMain)
             .fillMaxSize()
     ) {
-        //cambiar cuando se tenga el view model de ordenes
-        Column {
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
             NavigationBar(navController = navController, tittleBar = stringResource(id = R.string.shoppingcart_page))
-            //CartAppBar(navController)
+            Divider()
+            Spacer(modifier = Modifier.size(20.dp))
 
-            //Lista de carrito de pruebas
-            val articlesOnCart: MutableList<CartItemArticleDetail> = mutableListOf()
-            val articleOne = CartItemArticleDetail(
-                "62544cad-3dfd-42b1-bc4d-18c09dfdaaf1",
-                "Articulo de prueba 1",
-                "https://m.media-amazon.com/images/I/61NGnpjoRDL._AC_SL1500_.jpg",
-                "Computers",
-                "MOCK1",
-                150.9F,
-                2
-            )
-            val articleTwo = CartItemArticleDetail(
-                "62544cad-3dfd-42b1-bc4d-18c09dfdaaf2",
-                "Articulo de prueba 2",
-                "https://m.media-amazon.com/images/I/61pUul1oDlL._AC_SL1500_.jpg",
-                "Accessories",
-                "MOCK1",
-                33.5F,
-                1
-            )
-            articlesOnCart.add(articleOne)
-            articlesOnCart.add(articleTwo)
-            ShoppingCartList(carItemList = articlesOnCart)
-            var subTotalPrice: Float = (0.0F)
-
-            for (i in articlesOnCart.indices) {
-                subTotalPrice = articlesOnCart[i].precioProducto * articlesOnCart[i].cantidadArticulos
+            if (orderProductsList.size == 0) {
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                    Text(
+                        text = stringResource(id = R.string.cart_is_empty),
+                        style = MaterialTheme.typography.caption,
+                        color = Color.White,
+                    )
+                }
             }
-            Log.d("prueba barra de precio", subTotalPrice.toString())
-            totalPriceBar(subTotalPrice)
+
+            for (product in orderProductsList) {
+                CardItemCart(product)
+            }
+            Spacer(modifier = Modifier.size(20.dp))
+            Divider()
+            Spacer(modifier = Modifier.size(10.dp))
+            CardFooterOrder(subTotalOrder, taxOrder, totalOrder)
+            Spacer(modifier = Modifier.size(10.dp))
+            Divider()
+            OutlinedButton(
+                onClick = { },
+                shape = RoundedCornerShape(4.dp),
+                enabled = true,
+                elevation = ButtonDefaults.elevation(
+                    defaultElevation = 10.dp,
+                    pressedElevation = 5.dp,
+                    disabledElevation = 0.dp,
+                ), modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 5.dp, end = 16.dp, bottom = 5.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Green,
+                    contentColor = Color.Black
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.create_order_text),
+                    modifier = Modifier.padding(6.dp)
+                )
+            }
         }
     }
 }
@@ -76,67 +105,113 @@ fun PreviewShoppingCartMainPage() {
 }
 
 @Composable
-fun totalPriceBar(subTotalOrderValue: Float) {
-    Column() {
+fun CardItemCart(productInCart: ProductoOrden) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 4.dp, vertical = 4.dp)
+            .fillMaxWidth()
+            .height(60.dp),
+        backgroundColor = BackgroundMain,
+        elevation = 4.dp,
+        shape = RoundedCornerShape(corner = CornerSize(5.dp))
+    ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(5.dp),
+                .fillMaxHeight(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.End
         ) {
-            Text(
-                text = stringResource(R.string.shopping_cart_subtotal_label),
-                style = MaterialTheme.typography.subtitle1,
-                color = Color.White
-            )
-            Text(
-                text = stringResource(R.string.cart_price, subTotalOrderValue.toString()),
-                style = MaterialTheme.typography.subtitle1,
-                color = Color.Yellow,
-                textDecoration = TextDecoration.Underline
-            )
-        }
-        Row(
-            modifier = Modifier
-                //.fillParentMaxWidth()
-                .fillMaxWidth()
-                .padding(5.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.End
-        ) {
-            Text(
-                text = stringResource(R.string.shopping_cart_tax_label),
-                style = MaterialTheme.typography.subtitle1,
-                color = Color.White
-            )
-            Text(
-                text = stringResource(R.string.cart_price, subTotalOrderValue.toString()),
-                style = MaterialTheme.typography.subtitle1,
-                color = Color.Yellow,
-                textDecoration = TextDecoration.Underline
-            )
-        }
-        Row(
-            modifier = Modifier
-                //.fillParentMaxWidth()
-                .fillMaxWidth()
-                .padding(5.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.End
-        ) {
-            Text(
-                text = stringResource(R.string.shopping_cart_total_label),
-                style = MaterialTheme.typography.subtitle1,
-                color = Color.White
-            )
-            Text(
-                text = stringResource(R.string.cart_price, subTotalOrderValue.toString()),
-                style = MaterialTheme.typography.subtitle1,
-                color = Color.Yellow,
-                textDecoration = TextDecoration.Underline
+            Column(modifier = Modifier.padding(end = 5.dp)) {
+                Text(
+                    text = productInCart.descripcionProducto,
+                    style = MaterialTheme.typography.caption,
+                    color = Color.White,
+                    maxLines = 1
+                )
+                Row(modifier = Modifier.align(Alignment.End)) {
+                    Text(
+                        text = stringResource(R.string.cart_quantity, productInCart.cantidadVendida),
+                        style = MaterialTheme.typography.caption,
+                        color = Color.Green
+                    )
+                    Text(
+                        text = stringResource(id = R.string.pesos) + productInCart.precioProducto,
+                        style = MaterialTheme.typography.caption,
+                        color = Color.Green,
+                        textDecoration = TextDecoration.Underline
+                    )
+                }
+            }
+            Icon(
+                imageVector = Icons.Filled.Delete,
+                contentDescription = stringResource(id = R.string.delete_product_text),
+                tint = Color.White
             )
         }
     }
-    Divider()
+}
+
+@Composable
+fun CardFooterOrder(subT: Float, taxes: Float, total: Float) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 5.dp, bottom = 5.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
+    ) {
+        Text(
+            text = stringResource(id = R.string.shopping_cart_subtotal_label),
+            style = MaterialTheme.typography.caption,
+            color = Color.White,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = stringResource(id = R.string.pesos) + subT,
+            style = MaterialTheme.typography.caption,
+            color = Color.White,
+            maxLines = 1
+        )
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 5.dp, bottom = 5.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
+    ) {
+        Text(
+            text = stringResource(id = R.string.shopping_cart_tax_label),
+            style = MaterialTheme.typography.caption,
+            color = Color.White,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = stringResource(id = R.string.pesos) + taxes,
+            style = MaterialTheme.typography.caption,
+            color = Color.White,
+            maxLines = 1
+        )
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 5.dp, bottom = 5.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
+    ) {
+        Text(
+            text = stringResource(id = R.string.shopping_cart_total_label),
+            style = MaterialTheme.typography.subtitle2,
+            color = Color.Yellow,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = stringResource(id = R.string.pesos) + total,
+            style = MaterialTheme.typography.subtitle2,
+            color = Color.Yellow,
+            maxLines = 1
+        )
+    }
 }
